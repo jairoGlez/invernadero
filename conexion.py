e27610205eadfe710031d8abcab79f032a8a9c5c
Usuario <Usuario>
2017-06-15 11:11:22 -0500

    se creo la funcion para insertar usuarios

--- conexion.py
+++ conexion.py
@@ -6,10 +6,14 @@ c = db.cursor()
 def mostrarUsuario():
     r = c.execute("SELECT * FROM usuario")
     r = r.fetchall()
-    print(r)
     
-    #for fila in r:
-     #   print(fila)
+    for fila in r:
+        print(fila)
+        
+def insertarUsuario(datos):
+    c.execute("INSERT INTO usuario (nombre, apellido1, apellido2, correo, password, tipo) VALUES (?,?,?,?,?,?)", (datos[0],datos[1],datos[2],datos[3],datos[4],datos[5]))
+    db.commit()
         
         
+insertarUsuario(["J","G","G","j@hotmail.com","pswrd",5])
 mostrarUsuario()
