5a1679d25414f8e26552339dc9751234655ace3c
Usuario <Usuario>
2017-06-19 10:45:37 -0500

    Se convirtio el codigo a clase

--- conexion.py
+++ conexion.py
@@ -1,19 +1,21 @@
 import sqlite3
 
-db = sqlite3.connect("invernadero.db")
-c = db.cursor()
+class invernadero:
+    db = sqlite3.connect("invernadero.db")
+    c = db.cursor()
 
-def mostrarUsuario():
-    r = c.execute("SELECT * FROM usuario")
-    r = r.fetchall()
-    
-    for fila in r:
-        print(fila)
+    def mostrarUsuario(self):
+        r = self.c.execute("SELECT * FROM usuario")
+        r = r.fetchall()
+        lista = []
+
+        for fila in r:
+            lista.append(fila)
+            
+        return lista;
+
+    def insertarUsuario(self,datos):
+        self.c.execute("INSERT INTO usuario (nombre, apellido1, apellido2, correo, password, tipo) VALUES (?,?,?,?,?,?)", (datos[0],datos[1],datos[2],datos[3],datos[4],datos[5]))
+        self.db.commit()
         
-def insertarUsuario(datos):
-    c.execute("INSERT INTO usuario (nombre, apellido1, apellido2, correo, password, tipo) VALUES (?,?,?,?,?,?)", (datos[0],datos[1],datos[2],datos[3],datos[4],datos[5]))
-    db.commit()
         
-        
-insertarUsuario(["J","G","G","j@hotmail.com","pswrd",5])
-mostrarUsuario()
