f1ad2c6fdfcb818689c12590f71024d7fa8dccef
Usuario <Usuario>
2017-06-22 10:52:02 -0500

    servidor muestra usuarios en el navegador

--- servidor.py
+++ servidor.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from conexion import invernadero
+
 app = Flask("bd")
+inv = invernadero()
 
 @app.route("/")
 def raiz():
-    return ("Hola")
+    return("Hola")
+
+@app.route("/usuarios")
+def usuarios():
+    usuarios = inv.mostrarUsuario()
+    print(usuarios)
+    respuesta = jsonify(usuarios)
+    return respuesta
 
 app.run()
