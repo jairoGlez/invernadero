1ca843c40b708116692150249865d0e8bcaf17de
Usuario <Usuario>
2017-06-28 11:13:42 -0500

    La base de datos acepta info desde la pagina

--- servidor.py
+++ servidor.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from conexion import invernadero
 
 app = Flask("bd")
@@ -14,6 +14,23 @@ def usuarios():
     print(usuarios)
     respuesta = jsonify(usuarios)
     respuesta.headers.add('Access-Control-Allow-Origin', '*')
+    return respuesta
+
+@app.route("/agregar_usuario", methods=['POST'])
+def agregar_usuario():
+    print(request.form)
+    nombre = request.form['nombre']
+    apellido1 = request.form['apellido1']
+    apellido2 = request.form['apellido2']
+    correo = request.form['correo']
+    password = request.form['password']
+    tipo = request.form['tipo']
+    
+    inv.insertarUsuario([nombre,apellido1,apellido2,correo,password,tipo])
+    
+    respuesta = jsonify({'status':'Ok'})
+    respuesta.headers.add('Access-Control-Allow-Origin', '*')
+    
     return respuesta
 
 app.run()
