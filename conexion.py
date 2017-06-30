844fa888a5ac8b4f1552aea7409e2951e7b9cc67
Usuario <Usuario>
2017-06-30 11:24:31 -0500

    metodo buscar

--- conexion.py
+++ conexion.py
@@ -18,4 +18,12 @@ class invernadero:
         self.c.execute("INSERT INTO usuario (nombre, apellido1, apellido2, correo, password, tipo) VALUES (?,?,?,?,?,?)", (datos[0],datos[1],datos[2],datos[3],datos[4],datos[5]))
         self.db.commit()
         
+    def buscar(self,palabra):
+        self.c.execute("SELECT * FROM usuario WHERE correo LIKE ?",("%"+palabra+"%",))
+        lista = []
         
+        for e in self.c:
+            usuario = {'id':e[0],'nombre':e[1],'apellido1':e[2],'apellido2':e[3],'correo':e[4],'password':e[5],"tipo":e[6]}
+            lista.append(usuario)
+            
+        return lista
